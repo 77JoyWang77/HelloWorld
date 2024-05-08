3bd351c8742831bdfa419b0884f2bd278920d03c
clairechein0627 <clairechien0627@gmail.com>
2024-05-09 02:10:21 +0800

    新增empty模式

--- app/src/main/java/com/example/helloworld/CakeGame.java
+++ app/src/main/java/com/example/helloworld/CakeGame.java
@@ -1,24 +1,29 @@
 package final_project;
 
 import java.util.*;
-import javafx.util.Duration;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.util.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
+import javafx.scene.input.*;
 import javafx.geometry.*;
-import javafx.animation.RotateTransition;
+import javafx.animation.*;
 
 public class CakeGame extends Application {
 
 
-    int[] index = {-1,-1};
+    final int x_max = 5;
+    final int y_max = 5;
+    final int new_cake_max = 4;
 
+
+    int[] index = {-1,-1};
 
     @Override
     public void start(Stage primaryStage) {
@@ -34,16 +39,16 @@ public class CakeGame extends Application {
         table.setVgap(10);
         table.setPadding(new Insets(10));
 
-        CakePane[][] cakes = new CakePane[4][5];
-        for (int i=0;i<4;i++) {
-            for(int j=0;j<5;j++) {
+        CakePane[][] cakes = new CakePane[x_max][y_max];
+        for (int i=0;i<x_max;i++) {
+            for(int j=0;j<y_max;j++) {
                 cakes[i][j] = new CakePane();
                 table.add(cakes[i][j], i, j);
             }
         }
 
 
-        CakePane[] new_cakes = new CakePane[4];
+        CakePane[] new_cakes = new CakePane[new_cake_max];
 
         for(int i=0;i<4;i++) {;
             new_cakes[i] = new CakePane();
@@ -54,14 +59,20 @@ public class CakeGame extends Application {
         new_add.setAlignment(Pos.CENTER);
         new_add.getChildren().addAll(new_cakes);
 
-        Button button = new Button("refresh");
+        Button button_refresh = new Button("refresh");
+        Button button_clear = new Button("clear");
+        VBox buttons = new VBox(10);
+        buttons.getChildren().addAll(button_refresh, button_clear);
+        buttons.setAlignment(Pos.BASELINE_RIGHT);
 
         pane.setCenter(table);
         pane.setBottom(new_add);
-        pane.setTop(button);
+        pane.setTop(buttons);
         pane.setPadding(new Insets(10));
 
-        BorderPane.setAlignment(button, Pos.CENTER_RIGHT);
+
+
+
 
         scene.setOnKeyPressed(event -> { //鍵盤按鍵事件
             if ((event.getCode() == KeyCode.A || event.getCode() == KeyCode.D || event.getCode() == KeyCode.W || event.getCode() == KeyCode.S) && index[0] == -1) {
@@ -77,7 +88,7 @@ public class CakeGame extends Application {
                 }
             }
             else if (event.getCode() == KeyCode.D) { //右鍵
-                if (index[0] < 3) {
+                if (index[0] < x_max-1) {
                     cakes[index[0]][index[1]].unchoosed();
                     cakes[index[0] + 1][index[1]].choosed();
                     index[0]++;
@@ -91,7 +102,7 @@ public class CakeGame extends Application {
                 }
             }
             else if (event.getCode() == KeyCode.S) { //下鍵
-                if (index[1] < 4) {
+                if (index[1] < y_max-1) {
                     cakes[index[0]][index[1]].unchoosed();
                     cakes[index[0]][index[1] + 1].choosed();
                     index[1]++;
@@ -99,35 +110,32 @@ public class CakeGame extends Application {
             }
         });
 
-        new_cakes[0].setOnMouseClicked(event -> {
-            if(index[0] != -1) {
-                new_cakes[0].put_cake_to_table(cakes[index[0]][index[1]], new_cakes[0]);
-                cakes[index[0]][index[1]].mix(cakes, cakes[index[0]][index[1]], index[0], index[1]);
-            }
-        });
-        new_cakes[1].setOnMouseClicked(event -> {
-            if(index[0] != -1) {
-                new_cakes[1].put_cake_to_table(cakes[index[0]][index[1]], new_cakes[1]);
-                cakes[index[0]][index[1]].mix(cakes, cakes[index[0]][index[1]], index[0], index[1]);
-            }
-        });
-        new_cakes[2].setOnMouseClicked(event -> {
-            if(index[0] != -1) {
-                new_cakes[2].put_cake_to_table(cakes[index[0]][index[1]], new_cakes[2]);
-                cakes[index[0]][index[1]].mix(cakes, cakes[index[0]][index[1]], index[0], index[1]);
-            }
-        });
-        new_cakes[3].setOnMouseClicked(event -> {
-            if(index[0] != -1) {
-                new_cakes[3].put_cake_to_table(cakes[index[0]][index[1]], new_cakes[3]);
-                cakes[index[0]][index[1]].mix(cakes, cakes[index[0]][index[1]], index[0], index[1]);
-            }
-        });
+        for(int i=0;i<new_cake_max;i++) {
+
+            final int a = i;
+
+            new_cakes[a].setOnMouseClicked(event -> {
+                if(index[0] != -1) {
+                    new_cakes[a].put_cake_to_table(cakes[index[0]][index[1]]);
+                    cakes[index[0]][index[1]].mix(cakes, index[0], index[1]);
+                }
+            });
+        }
 
 
-        button.setOnAction(event -> {
-            for(int i=0;i<4;i++) {
+
+
+        button_refresh.setOnAction(event -> {
+            for(int i=0;i<new_cake_max;i++) {
                 new_cakes[i].refresh();
+            }
+        });
+
+        button_clear.setOnAction(event -> {
+            for(int i=0;i<x_max;i++) {
+                for(int j=0;j<y_max;j++) {
+                    cakes[i][j].clearPieces();
+                }
             }
         });
 
--- app/src/main/java/com/example/helloworld/CakePane.java
+++ app/src/main/java/com/example/helloworld/CakePane.java
@@ -1,30 +1,41 @@
 package final_project;
 
 import java.util.*;
-import javafx.util.Duration;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.stage.Stage;
+import javafx.util.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
+import javafx.scene.input.*;
 import javafx.geometry.*;
-import javafx.animation.KeyFrame;
-import javafx.animation.RotateTransition;
-import javafx.animation.Timeline;;
+import javafx.animation.*;
+
 
 public class CakePane extends StackPane {
 
+    private final int x_max = 5;
+    private final int y_max = 5;
+    private final int pieces_kind = 6;
+    private final int pieces_max = 8;
+
+
+    private final int[][] position = {{1,0},{2,0},{0,1},{3,1},{0,2},{3,2},{1,3},{2,3}};	//放置蛋糕片的座標
+    private final int[][] dir = {{0,-1},{-1, 0},{0,1},{1,0},{0,0}};				//蛋糕相對位置上左下右中
+    private final String[] dir_str = {"top", "left", "bottom", "right", "mid"};	//蛋糕相對位置上左下右中string版
+
+
     private Circle circle = new Circle(35);
-    private GridPane whole_cake = new GridPane();
-    private ArrayList<Integer> pieces = new ArrayList<>();
-    private int[] pieces_num = {0, 0, 0, 0, 0, 0};
-    private int[][] position = {{1,0},{2,0},{0,1},{3,1},{0,2},{3,2},{1,3},{2,3}};
+    private GridPane whole_cake = new GridPane();			//按座標放置的蛋糕片
+    private ArrayList<Integer> pieces = new ArrayList<>();	//蛋糕片花色ArrayList版
+    private int[] pieces_num = new int[pieces_kind];		//6種花色的各自數量array版
+
+
+
 
     public CakePane() {
         getChildren().add(circle);
@@ -33,6 +44,9 @@ public class CakePane extends StackPane {
         circle.setFill(Color.WHITE);
         whole_cake.setHgap(6);
         whole_cake.setVgap(0.5);
+        for(int i=0;i<pieces_kind;i++) {
+            pieces_num[i] = 0;
+        }
         StackPane.setAlignment(whole_cake, Pos.CENTER);
     }
 
@@ -41,13 +55,17 @@ public class CakePane extends StackPane {
         pieces.add(p);
         pieces_num[p]++;
         Collections.sort(pieces, Comparator.naturalOrder());
-        for(int i=0;i<8;i++) {
+        for(int i=0;i<pieces_max;i++) {
             if(i<pieces.size()) {
                 whole_cake.add(new Label(Integer.toString(pieces.get(i))), position[i][0] + 2, position[i][1] + 5);
             }
             else {
                 whole_cake.add(new Label(" "), position[i][0] + 2, position[i][1] + 5);
             }
+        }
+
+        if(pieces.size() == 8) {
+            full();
         }
     }
 
@@ -55,7 +73,7 @@ public class CakePane extends StackPane {
         whole_cake.getChildren().clear();
         pieces.remove(Integer.valueOf(p));
         pieces_num[p]--;
-        for(int i=0;i<8;i++) {
+        for(int i=0;i<pieces_max;i++) {
             if(i<pieces.size()) {
                 whole_cake.add(new Label(Integer.toString(pieces.get(i))), position[i][0] + 2, position[i][1] + 5);
             }
@@ -67,104 +85,101 @@ public class CakePane extends StackPane {
 
 
 
-    public void put_cake_to_table(CakePane plate, CakePane new_cake) {
+    public void put_cake_to_table(CakePane plate) {
         if(plate.getPieces().size() > 0) {
             return;
         }
 
-        for(int i=0;i<new_cake.getPieces().size();i++) {
-            plate.place_piece(new_cake.getPieces().get(i));
+        for(int i=0;i<getPieces().size();i++) {
+            plate.place_piece(getPieces().get(i));
         }
     }
 
-    public void mix(CakePane[][] cakes, CakePane new_cake, int x, int y) {
-        for(int i=0;i<6;i++) {
-            if(new_cake.getPiecesNum(i) > 0){
-                if(new_cake.getPieces().size() == new_cake.getPiecesNum(i)) {
-                    place_mid(cakes, new_cake, x, y, i);
-                }
+
+
+
+    public void mix(CakePane[][] cakes, int x, int y) {
+
+        for(int i=0;i<pieces_kind;i++) {
+            String mode = situation(cakes, x, y, i)[0];
+            int place = Integer.parseInt(situation(cakes, x, y, i)[1]);
+
+            if(mode.equals("empty")) {
+
+                place_piece(cakes, x + dir[place][0], y + dir[place][1], i, -1);
+
+            }
+            else {
+
             }
         }
     }
 
-    public void place_mid(CakePane[][] cakes, CakePane new_cake, int x, int y, int p) {
-        int num = new_cake.getPiecesNum(p);
-        if(y>0 && cakes[x][y-1].getPiecesNum(p) > 0) {
-            int from_num = cakes[x][y-1].getPiecesNum(p);
-            if(from_num + num >= 8) {
-                for(int i=0;i<8 - num;i++) {
-                    move(new_cake, cakes[x][y-1], p);
-                }
-                new_cake.full();
-                return;
+
+
+
+
+
+    public void place_piece(CakePane[][] cakes, int x, int y, int p, int from) {
+        for(int i=0;i<4;i++) {	//上左下右
+            if(i != -1 && i == (from + 2) % 4) {
+                continue;
             }
-            else {
-                for(int i=0;i<from_num;i++) {
-                    move(new_cake, cakes[x][y-1], p);
+
+            int num = cakes[x][y].getPieces().size();
+
+            for(int j=0;j<pieces_max - num;j++) {	//要放到的位置的空格數
+                if(cakes[x + dir[i][0]][y + dir[i][1]].getPiecesNum(p) == 0) {
+                    break;
                 }
-            }
-        }
-        if(x>0 && cakes[x-1][y].getPiecesNum(p) > 0) {
-            int from_num = cakes[x-1][y].getPiecesNum(p);
-            if(from_num + num >= 8) {
-                for(int i=0;i<8 - num;i++) {
-                    move(new_cake, cakes[x-1][y], p);
-                }
-                new_cake.full();
-                return;
-            }
-            else {
-                for(int i=0;i<from_num;i++) {
-                    move(new_cake, cakes[x-1][y], p);
-                }
-            }
-        }
-        if(y<4 && cakes[x][y+1].getPiecesNum(p) > 0) {
-            int from_num = cakes[x][y+1].getPiecesNum(p);
-            if(from_num + num >= 8) {
-                for(int i=0;i<8 - num;i++) {
-                    move(new_cake, cakes[x][y+1], p);
-                }
-                new_cake.full();
-                return;
-            }
-            else {
-                for(int i=0;i<from_num;i++) {
-                    move(new_cake, cakes[x][y+1], p);
-                }
-            }
-        }
-        if(x<3 && cakes[x+1][y].getPiecesNum(p) > 0) {
-            int from_num = cakes[x+1][y].getPiecesNum(p);
-            if(from_num + num >= 8) {
-                for(int i=0;i<8 - num;i++) {
-                    move(new_cake, cakes[x+1][y], p);
-                }
-                new_cake.full();
-                return;
-            }
-            else {
-                for(int i=0;i<from_num;i++) {
-                    move(new_cake, cakes[x+1][y], p);
-                }
+
+                place_piece(cakes, x + dir[i][0], y + dir[i][1], p, i);
+
+                move(cakes[x][y], cakes[x + dir[i][0]][y + dir[i][1]], p);
             }
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+    public String[] situation(CakePane[][] cakes, int x, int y, int p) {
+        String[] result = {"","-1"};
+        for(int i=0;i<5;i++) {
+            if(cakes[x + dir[i][0]][y + dir[i][1]].getPieces().size() > 0 && cakes[x + dir[i][0]][y + dir[i][1]].getPieces().size() == cakes[x + dir[i][0]][y + dir[i][1]].getPiecesNum(p)) {
+                result[0] = "empty";
+                result[1] = Integer.toString(i);
+
+                return result;
+            }
+        }
+        return result;
+    }
+
+
 
 
     public void full() {
-        if(pieces.size() == 8) {
+        if(pieces.size() == pieces_max) {
             clearPieces();
+            circle.setFill(Color.BLUE);
             EventHandler<ActionEvent> eventHandler = e -> {
                 if(circle.getFill() != Color.BLUE) {
                     circle.setFill(Color.BLUE);
                 }
                 else {
-                    circle.setFill(Color.BLUE);
+                    circle.setFill(Color.WHITE);
                 }
             };
             Timeline animation = new Timeline(new KeyFrame(Duration.millis(500), eventHandler));
-            animation.setCycleCount(2);
+            animation.setCycleCount(1);
             animation.play();
         }
     }
@@ -178,7 +193,7 @@ public class CakePane extends StackPane {
 
     public void clearPieces() {
         whole_cake.getChildren().clear();
-        for(int i=0;i<6;i++) {
+        for(int i=0;i<pieces_kind;i++) {
             pieces_num[i] = 0;
         }
         pieces.removeAll(pieces);
@@ -186,17 +201,43 @@ public class CakePane extends StackPane {
 
     public void refresh() {
         clearPieces();
-        int randomNumCake = (int) (Math.random() * 7 % 7) + 1;
+        int randomNumCake = (int) (Math.random() * (pieces_max-1) % (pieces_max-1)) + 1;
 
         for(int j=0;j<randomNumCake;j++) {
-            int randomCake = (int) (Math.random() * 6 % 6);
+            int randomCake = (int) (Math.random() * pieces_kind % pieces_kind);
 
             place_piece(randomCake);
 
         }
     }
 
-
+    public String positon(int x, int y) {
+        if(x == 0 && y == 0){
+            return "left_top";
+        }
+        else if(x == 0 && y == y_max-1) {
+            return "left_bottom";
+        }
+        else if(x == x_max-1 && y == 0) {
+            return "right_top";
+        }
+        else if(x == x_max-1 && y == y_max-1) {
+            return "right_bottom";
+        }
+        else if(x == 0) {
+            return "left";
+        }
+        else if(y == 0) {
+            return "top";
+        }
+        else if(x == x_max-1) {
+            return "right";
+        }
+        else if(y == y_max-1) {
+            return "bottom";
+        }
+        return "";
+    }
 
 
     public void choosed() {
